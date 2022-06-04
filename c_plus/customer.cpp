4f19e5e662056af2b940866daf3c2a1f45a83567
Fateh Singh <fateh@maropost.com>
2022-06-04 19:04:51 +0530

    add  customer c++

---
+++ c_plus/customer.cpp
@@ -0,0 +1,54 @@
+/*
+Class:
+A class in C++ is the building block that leads to Object-Oriented programming.
+It is a user-defined data type, which holds its own data members and member functions,
+which can be accessed and used by creating an instance of that class. A C++ class is
+like a blueprint for an object.
+
+An Object is an instance of a Class. When a class is defined, no memory is
+allocated but when it is instantiated (i.e. an object is created) memory is
+allocated.
+
+=======================
+compile file - g++ c_plus/customer.cpp -o bin/customer
+run this file: - ./bin/customer
+*/
+
+
+
+using namespace std;
+#include <iostream>
+#include <string>
+#include "./header/customer.h"
+
+// Definition of getFirstName using scope resolution operator ::
+void Customer::setFirstName() {
+  cout << "Enter First Name: ";
+  cin >> first_name;
+}
+
+void Customer::setLastName() {
+  cout << "Enter Last Name: ";
+  cin >> last_name;
+}
+
+int main(void){
+
+  Customer c1;
+  cout <<"=========Set Customer information ==========="<<endl;
+  c1.setFirstName();
+  c1.setLastName();
+  c1.order_id = 123;
+  c1.contact_number = "9855023924";
+  c1.address = "Manak Pur Sharif";
+
+
+  cout << endl<< endl;
+  cout <<"=========Show Customer information ==========="<<endl;
+  c1.getFullName();
+  cout << "Order ID: " << c1.order_id << endl;
+  cout << "Contact Number: " << c1.contact_number << endl;
+  cout << "Address: " << c1.address << endl;
+
+  return 0;
+}
---
+++ c_plus/header/customer.h
@@ -0,0 +1,35 @@
+//https://stackoverflow.com/questions/9579930/separating-class-code-into-a-header-and-cpp-file
+// A2DD.h
+#ifndef CUSTOMER_H
+
+  class Customer {
+    // Private data members
+    private:
+    char first_name[20]; // 1 Byte
+    char last_name[20]; // 1 Byte
+
+    public:
+    int order_id; // 2 Byte
+    string contact_number;
+    string address;
+
+    public:
+    // Inside class definition
+    void getFirstName(){
+      cout << "First Name: " << first_name << endl;
+    }
+
+    void getLastName(){
+      cout << "Last Name: " << last_name << endl;
+    }
+
+    void getFullName(){
+      cout << "Full Name: " << first_name << " " << last_name << endl;
+    }
+
+    // getFirstName is not defined inside class definition
+    void setFirstName();
+    void setLastName();
+  };
+
+#endif
